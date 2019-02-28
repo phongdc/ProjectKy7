747dfebca09aedaa6c98d13a9b3fc03c576cd430
phongdc <phongdc2010@gmail.com>
2019-02-28 14:21:45 +0700

    HttpUntils

---
+++ PeaApp/app/src/main/java/com/example/phongdc/peaapp/EmpDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.phongdc.peaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmpDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emp_details);
+    }
+}
