d31180237ca96c8e158f79e5c37bfc3f30658574
HaiVT9 <haivtse62609@fpt.edu.vn>
2019-03-28 15:43:19 +0700

    Done Register exception

--- PeaApp/app/src/main/java/com/example/phongdc/peaapp/Login/RegisterEmployeeActivity.java
+++ PeaApp/app/src/main/java/com/example/phongdc/peaapp/Login/RegisterEmployeeActivity.java
@@ -1,6 +1,8 @@
 package com.example.phongdc.peaapp.Login;
 
 import android.content.Intent;
+import android.service.autofill.RegexValidator;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +11,11 @@ import android.widget.Toast;
 import com.example.phongdc.peaapp.R;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
 import customfonts.MyEditText;
 import cz.msebera.android.httpclient.Header;
 
@@ -33,39 +39,56 @@ public class RegisterEmployeeActivity extends AppCompatActivity {
         }
         return true;
     }
+    private boolean passWordValid(String data){
+        if(!data.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")){
+            Toast.makeText(RegisterEmployeeActivity.this, "Xin điền đúng mật khẩu !!!", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
     public void clickToCreateAccountEmployee(View view) {
         String mEmail = tvMailRegister.getText().toString();
         String mPassword = tvPasswordRegister.getText().toString();
         String mName = tvNameRegister.getText().toString();
         String mAddress = tvAddressRegister.getText().toString();
         String mPhone = tvPhoneRegister.getText().toString();
-        if (valid(mEmail) && valid(mPassword) && valid(mName) && valid(mAddress) && valid(mPhone)) {
+        if (valid(mEmail)  && valid(mPassword) && valid(mName) && valid(mAddress) && valid(mPhone) && passWordValid(mPassword)) {
             AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
             final RequestParams params = new RequestParams();
+
             params.put("email", mEmail);
             params.put("password", mPassword);
+            params.put("store_id", 1);
             params.put("name", mName);
             params.put("address", mAddress);
             params.put("phone", mPhone);
             params.setUseJsonStreamer(true);
 
-            asyncHttpClient.post("http://payroll.unicode.edu.vn/api/user/register", params, new AsyncHttpResponseHandler() {
+            asyncHttpClient.post("http://payroll.unicode.edu.vn/api/user/register", params, new JsonHttpResponseHandler() {
                 @Override
                 public void onStart() {
                 }
 
+
+
                 @Override
-                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                    Toast.makeText(RegisterEmployeeActivity.this, "Duyệt Thành Công", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(RegisterEmployeeActivity.this, LoginActivity.class);
-                    startActivity(intent);
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    try {
+                        if (response.getBoolean("success") == false) {
+                            Toast.makeText(RegisterEmployeeActivity.this, "Tạo thất bại", Toast.LENGTH_SHORT).show();
+                        }else {
+                        Toast.makeText(RegisterEmployeeActivity.this, "Tạo thành công", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(RegisterEmployeeActivity.this, LoginActivity.class);
+                        startActivity(intent);}
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
                 }
 
                 @Override
-                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                    Toast.makeText(RegisterEmployeeActivity.this, "Thêm thất bại", Toast.LENGTH_SHORT).show();
+                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                    super.onFailure(statusCode, headers, throwable, errorResponse);
                 }
-
 
                 @Override
                 public void onRetry(int retryNo) {
@@ -75,5 +98,6 @@ public class RegisterEmployeeActivity extends AppCompatActivity {
         }else {
             Toast.makeText(RegisterEmployeeActivity.this, "Xin điền đầy đủ !!", Toast.LENGTH_SHORT).show();
         }
+
     }
 }
